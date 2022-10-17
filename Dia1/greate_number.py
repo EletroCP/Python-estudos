b0f251dfc30b3e8bcdb56c38328290227029d282
eletrocp <brernolopes@hotmail.com>
2022-10-17 18:25:10 -0300

    :sparkles: feat: função que retorna o maior numero entre eles

---
+++ Dia1/greate_number.py
@@ -0,0 +1,8 @@
+def greate_number(num1, num2):
+    if(num1 > num2): return num1
+    return num2
+
+print(greate_number(10, 15))
+print(greate_number(10, -15))
+print(greate_number(-10, -15))
+print(greate_number(1.1, 1.2))
